cfc29c7d0fbc3e0979d428fcf4e1c20b02f15106
Kathleen Ohde <Kathleen.Ohde1@t-mobile.com>
2019-12-03 16:03:29 -0600

    init commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <TextInput />
+        <Button title="ADD" />
+      </View>
+      <View>
+        
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
