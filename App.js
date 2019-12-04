84eae2a5d926edaa4581a5e280652be2552fc913
Kathleen Ohde <Kathleen.Ohde1@t-mobile.com>
2019-12-04 11:33:44 -0600

    typo

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <TextInput />
+    <View style={{ padding: 50 }}>
+      <View style={{flexDirection: 'row', justifyContent: "space-between", alignItems: 'center'}}>
+        <TextInput        
+          placeholder="Course Goal" 
+          style={{borderBottomColor: 'black', borderBottomWidth: 1, padding: 10, width: '80%'}}/>
         <Button title="ADD" />
       </View>
       <View>
